b193ff45215700c7a1f5a59cc486ded02f629602
Lawson Kurtz <lawson.kurtz@viget.com>
2013-05-01 11:33:13 -0400

    Clean up js

--- github_review.js
+++ github_review.js
@@ -1,14 +1,15 @@
 $(function(){
   $('body').append("<div class='github_review'><form><input class='filename' type='text'  placeholder='Pull up some more diffs.' size='50' /></form><div id='files' class='files'></div></div>");
 
-  $('.github_review input.filename').on('keyup', function(){
-    $file_container = $('.github_review .files');
+  $filename_input = $('.github_review input.filename');
+  $file_container = $('.github_review .files');
+
+  $filename_input.on('keyup', function(){
     $file_container.empty();
-    filename = $('.github_review input.filename').val();
+    filename = $filename_input.val();
     files = $('#files .file .meta[data-path*="' + filename + '"]').parent().clone();
 
     if(files.length > 0){
-      console.log(files);
       files.appendTo($file_container)
     }
   });
